1d4a02726e803f98bd313b0049fdd0459bfd0cb9
dujianwei <hellcow@126.com>
2018-12-07 17:17:47 +0800

    add export logic

--- src/codeexport/views/MainView.java
+++ src/codeexport/views/MainView.java
@@ -1,77 +1,55 @@
 package codeexport.views;
 
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.DoubleClickEvent;
-import org.eclipse.jface.viewers.IDoubleClickListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.ViewPart;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.part.ViewPart;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
 public class MainView extends ViewPart {
+
 	public MainView() {
+		document = new XWPFDocument();
 	}
 
 	/**
@@ -79,96 +57,184 @@ public class MainView extends ViewPart {
 	 */
 	public static final String ID = "codeexport.views.MainView";
 
-	@Inject IWorkbench workbench;
-	private Text text;
+	@Inject
+	IWorkbench workbench;
+	private Text inputDirText;
 
-	private Button btnNewButton;
-	 
+	private Button inputButton;
+
+	private XWPFDocument document;
+	private Text exportFilePathText;
+
+	private Button exportButton;
+
+	private Button exportFileButton;
 
 	/**
 	 * 2007word文档创建
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
-	public void createWord2007() throws IOException {
-		XWPFDocument document= new XWPFDocument();
-
-        //Write the Document in file system
-        FileOutputStream out = new FileOutputStream(new File("/Users/jv/Desktop/create_toc.docx"));
-
-        //添加标题
-        XWPFParagraph titleParagraph = document.createParagraph();
-
-        //设置段落居中
-        titleParagraph.setAlignment(ParagraphAlignment.CENTER);
-
-        XWPFRun titleParagraphRun = titleParagraph.createRun();
-        titleParagraphRun.setText("Java PoI");
-        titleParagraphRun.setColor("000000");
-        titleParagraphRun.setFontSize(20);
-
-        //段落
-        XWPFParagraph firstParagraph = document.createParagraph();
-        firstParagraph.setStyle("Heading1");
-        XWPFRun run = firstParagraph.createRun();
-        run.setText("段落1。");
-        run.setColor("696969");
-        run.setFontSize(18);
-
-
-        //段落
-        XWPFParagraph firstParagraph1 = document.createParagraph();
-        firstParagraph.setStyle("Heading1");
-        XWPFRun run1 = firstParagraph1.createRun();
-        run1.setText("段落2");
-        run1.setColor("696969");
-        run1.setFontSize(16);
-
-        document.createTOC();
-
-        document.write(out);
-        out.close();
+	private void createWord2007() throws IOException {
+		
+		
+		inputDirText.getText();
+		String[] fileFilter = new String[] {"m"};
+		Collection<File> pathes = FileUtils.listFiles(new File(inputDirText.getText()), fileFilter, true);
+		
+//		txt
+		File f = new File("/Users/jv/Desktop/123.txt");
+		for (File file : pathes) {
+			StringBuffer sb = new StringBuffer();
+			List<String> readLines = FileUtils.readLines(file, "UTF-8");
+			for (int i = 0; i < readLines.size(); i++) {
+				String string = readLines.get(i);
+				sb.append(string).append(System.getProperty("line.separator"));
+			}
+			
+			String result = sb.toString();
+			//去掉注释
+			///\*[\w\W]*?\*/
+			////.*
+			result = result.replaceAll("\\*[\\w\\W]*?\\*/", "");
+			result = result.replaceAll("//.*", "");
+			
+			//去掉空白行
+			result = result.replaceAll("^(\\s*)\\r\\n", "");
+			result = result.replaceAll("^(\\s*)\\n", "");
+			
+			FileUtils.write(f, result, "UTF-8", true);
+		}
+		
+//		word
+//		// Write the Document in file system
+//		FileOutputStream out = new FileOutputStream(new File("/Users/jv/Desktop/create_toc.docx"));
+//
+//
+//		// 段落
+//		for (File file : pathes) {
+//			XWPFParagraph firstParagraph = document.createParagraph();
+//			firstParagraph.setSpacingAfter(0);
+//			firstParagraph.setSpacingBefore(0);
+//			
+//			XWPFRun run = firstParagraph.createRun();
+//			StringBuffer sb = new StringBuffer();
+//			List<String> readLines = FileUtils.readLines(file, "UTF-8");
+//			for (int i = 0; i < readLines.size(); i++) {
+//				String string = readLines.get(i);
+//				sb.append(string).append(System.getProperty("line.separator"));
+//			}
+//			run.setText(new String(sb));
+//			run.setColor("696969");
+//			run.setFontSize(18);
+//		}
+//		
+//
+//
+//		document.createTOC();
+//
+//		document.write(out);
+//		out.close();
+//		
+//		inputDirText.setEnabled(true);
+//		exportFilePathText.setEnabled(true);
+//		exportButton.setEnabled(true);
+//		inputButton.setEnabled(true);
+//		exportFileButton.setEnabled(true);
 	}
 
+	private void startExport() throws IOException {
+
+		if (inputDirText.getText().isEmpty()) {
+
+			MessageDialog.openError(null, "提示", "根目录为空");
+			
+			inputButton.notifyListeners(SWT.Selection, new Event());
+			
+			return;
+		}
+		
+		if (exportFilePathText.getText().isEmpty()) {
+
+			MessageDialog.openError(null, "提示", "导出目录为空");
+			exportButton.notifyListeners(SWT.Selection, new Event());
+			
+			return;
+		}
+		
+		inputDirText.setEnabled(false);
+		exportFilePathText.setEnabled(false);
+		exportButton.setEnabled(false);
+		inputButton.setEnabled(false);
+		exportFileButton.setEnabled(false);
+
+		createWord2007();
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout(3, false));
-		
+
 		Label fileLabel = new Label(parent, SWT.NONE);
-		fileLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		fileLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
 		fileLabel.setText("目录路径");
-		
-		text = new Text(parent, SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
-		btnNewButton = new Button(parent, SWT.NONE);
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
+
+		inputDirText = new Text(parent, SWT.BORDER);
+		inputDirText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		inputButton = new Button(parent, SWT.NONE);
+		inputButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				DirectoryDialog dd = new DirectoryDialog(workbench.getDisplay().getActiveShell());
 				String path = dd.open();
-				text.setText(path);
+				if(path!= null && !path.isEmpty())
+				{
+					inputDirText.setText(path);
+				}
 			}
 		});
-		btnNewButton.setText("选择目录");
-		
-		
-		Button button = new Button(parent, SWT.NONE);
-		button.addSelectionListener(new SelectionAdapter() {
+		inputButton.setText("项目根目录");
+
+		Label label = new Label(parent, SWT.NONE);
+		label.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		label.setText("导出文件目录");
+
+		exportFilePathText = new Text(parent, SWT.BORDER);
+		exportFilePathText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		exportButton = new Button(parent, SWT.NONE);
+		exportButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dd = new DirectoryDialog(workbench.getDisplay().getActiveShell());
+				String path = dd.open();
+				if (path != null && !path.isEmpty()) {
+					exportFilePathText.setText(path);
+				}
 			}
 		});
-		button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
-		button.setText("开始");
-		
-		
-		
-	}
+		exportButton.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		exportButton.setText("导出目录");
 
+		exportFileButton = new Button(parent, SWT.NONE);
+		exportFileButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					startExport();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		exportFileButton.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		exportFileButton.setText("开始");
+
+	}
 
 	@Override
 	public void setFocus() {
-		btnNewButton.setFocus();
+		inputButton.setFocus();
 	}
 }
